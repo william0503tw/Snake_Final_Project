e3d20c08b1202a20ca48b5dc4bdb9002c19ff676
William <william0503tw@gmail.com>
2021-12-10 22:40:18 +0800

    Latest Commit(12/10) stable version, drawing body left with comments

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ public:
     int length = 3;
     deque<int> body_x = {25,25,25}; //init position
     deque<int> body_y = {13,14,15}; //init position
-    int speed = 900; //990 full speed
+    int speed = 860; //990 full speedss
 
     //character movement
     void updateSnake(int direction);
@@ -64,13 +64,13 @@ void snake::indexObstacle(int y , int x_start , int x_finish){
 void snake::initPlayground()
 {
     //In this function, it will generate random location food, obstacles,
-    //infill GROUND
+    //場地座標 infill GROUND
     for(int i = 1 ; i <= x_size-1 ; i++){
         for(int j = 1 ; j <= y_size-1 ; j++) {
             ground[i][j] = GROUND;
         }
     }
-    //build WALL
+    //牆壁座標 build WALL
     for(int i = 0 ; i <= y_size-1 ; i++){
         ground[0][i] = WALL ;
     }
@@ -87,8 +87,7 @@ void snake::initPlayground()
     for(int i = 0 ; i <= x_size-1 ; i++){
         ground[i][26] = WALL ;
     }
-
-    //obstacles
+    //障礙物座標 obstacles
     indexObstacle(5,6,13);
     indexObstacle(6,6,8);
     indexObstacle(7,4,8);
@@ -112,16 +111,21 @@ void snake::initPlayground()
     indexObstacle(14,10,19);
     indexObstacle(15,12,17);
 
-    //food
+    //隨機建立食物座標 food
     int temp ;
     srand((unsigned int)time(NULL));
-    for(int i = 1 ; i <= FOOD_COUNT ; i++){
-        if(temp != rand()){
-            int x_ran = rand() % (x_size - 2) + 1 ;
-            int y_ran = rand() % (y_size - 2) + 2 ;
-            if(ground[x_ran][y_ran] != OBSTACLE && ground[x_ran][y_ran] != WALL){
+    for(int i = 1 ; i <= FOOD_COUNT ; i++)
+    {
+        if(temp != rand())
+        {
+            int x_ran = rand() % (x_size - 2) + 1 ; // x = 1~50
+            int y_ran = rand() % (y_size - 2) + 2 ; // y = 2~25
+            if(ground[x_ran][y_ran] != OBSTACLE && ground[x_ran][y_ran] != WALL) //座標非障礙物且非牆壁
+            {
                 ground[x_ran][y_ran] = FOOD ;
-            }else{
+            }
+            else
+            {
                 ground[x_ran][y_ran] = OBSTACLE ;
                 i--;
             }
@@ -139,13 +143,16 @@ void snake::initPlayground()
         temp = rand();
     }
 }
-void snake::setScore(int s) {
-    cout << "\b \b";
-    gotoXY(28,-4);
-    cout << score ;
-    gotoXY(x,y);
+void snake::setScore(int s)
+{
+    cout << "\b \b";        //清空分數
+    gotoXY(28,-4);    //游標到分數的位置
+    cout << score ;         //更新分數
+    gotoXY(x,y);           //游標回到蛇頭位置
 }
-void snake::drawScore(){
+
+void snake::drawScore() //畫出分數
+{
     cout << string(15,' ') << "////////////////////" << endl;
     cout << string(15,' ') << setfill(' ') << "//" << setw(18) << setfill(' ')  << "//" << endl;
     cout << string(15,' ') <<"//" << "    " << "Score: " << score  << "    "  << "//" << endl;
@@ -153,12 +160,16 @@ void snake::drawScore(){
     cout << string(15,' ') <<"////////////////////" << endl;
     cout << endl ;
 }
-void snake::drawGround()
+
+void snake::drawGround()  //畫出場地
 {
-    clearScreen();
-    drawScore();
-    for(int i = 0 ; i <= y_size-1 ; i++){
-        for(int j = 0 ; j <= x_size-1 ; j++){
+    clearScreen(); //清空屏幕
+    drawScore();   //畫出分數
+    //畫出牆壁、食物、障礙物
+    for(int i = 0 ; i <= y_size-1 ; i++)
+    {
+        for(int j = 0 ; j <= x_size-1 ; j++)
+        {
             if(ground[j][i] == WALL){
                 SetConsoleTextAttribute(hConsole, 11);
                 cout << "+"   ;
@@ -180,20 +191,24 @@ void snake::drawGround()
     cout << "@" ;
     resetColor();
 }
-int snake::onTouch(int x, int y) {
-    return ground[x][y] ;
+int snake::onTouch(int x, int y)
+{
+    return ground[x][y] ; //回傳位置座標
 }
-void snake::updateSnake(int direction){
-    if( (x < x_size-1 && x > 0) && (y < y_size-1 && y > 0) ){
+
+void snake::updateSnake(int direction)
+{
+    if( (x < x_size-1 && x > 0) && (y < y_size-1 && y > 0) )
+    {
         switch(direction){
             case UP :
-                cout <<"\b \b";
+                cout <<"\b \b"; //清除蛇
                 y--;
-                gotoXY(x,y);
+                gotoXY(x,y);    //游標更新
                 SetConsoleTextAttribute(hConsole, 10);
-                cout << "@" ;
+                cout << "@" ;   //重新畫出蛇
                 resetColor();
-                Sleep(1000 - speed);
+                Sleep(1000 - speed);  //暫停程式(1000-speed)毫秒
                 break;
 
             case DOWN :
@@ -233,18 +248,22 @@ void snake::updateSnake(int direction){
 
     }
 }
-void snake::recreateFood(){
+void snake::recreateFood() //蛇吃到食物後，新增另一個食物的位置
+{
     newRandom:
-    int new_x = rand() % (x_size - 2) + 2 ; //2~50
-    int new_y = rand() % (y_size - 2)  ; //2~25
-    if(ground[new_x][new_y] != OBSTACLE && ground[new_x][new_y] != WALL){
+    int new_x = rand() % (x_size - 2) + 2 ; // x = 2~50
+    int new_y = rand() % (y_size - 2)  ;    // y = 2~25
+    if(ground[new_x][new_y] != OBSTACLE && ground[new_x][new_y] != WALL)
+    {
         ground[new_x][new_y] = FOOD;
         gotoXY(new_x,new_y);
         SetConsoleTextAttribute(hConsole, 6);
         cout << '&' ;
         resetColor();
         gotoXY(x,y);
-    }else{
+    }
+    else
+    {
         srand(rand());
         goto newRandom;
     }
@@ -255,55 +274,71 @@ void snake::recreateFood(){
 
 //main function
 int main() {
-    setWindowSize(450,600);
-    ShowConsoleCursor(false);
+    setWindowSize(450,600);  //遊戲視窗大小
+    ShowConsoleCursor(false);    //隱藏游標
     snake Snake;
 
-    Snake.initPlayground();
-    Snake.drawGround();
+    Snake.initPlayground();  //設定出場地、牆壁、障礙物的座標
+    Snake.drawGround();      //畫出場地、牆壁、障礙物
     _beginthread(readInput,0,(void*)0);
 
     int food_count = 0 ;
-    while(!gameOver){
-        Snake.updateSnake(direction);
-        if(Snake.onTouch(Snake.x,Snake.y) == OBSTACLE || Snake.onTouch(Snake.x,Snake.y) == WALL || Snake.onTouch(Snake.x,Snake.y) == BODY || Snake.onTouch(Snake.x,Snake.y) == BODY){
+    while(!gameOver)
+    {
+        Snake.updateSnake(direction); //根據使用者的input來移動蛇
+
+        if(Snake.onTouch(Snake.x,Snake.y) == OBSTACLE || Snake.onTouch(Snake.x,Snake.y) == WALL || Snake.onTouch(Snake.x,Snake.y) == BODY)
+        //蛇碰到障礙物、牆壁、蛇身 => GAMEOVER
+        {
             gameOver = true;
-        }else if(Snake.onTouch(Snake.x,Snake.y) == FOOD){
+        }
+        else if(Snake.onTouch(Snake.x,Snake.y) == FOOD)
+        //蛇吃到食物 => score++
+        {
             Snake.ground[Snake.x][Snake.y] = GROUND;
             Snake.setScore(Snake.score++);
             Snake.recreateFood();
             food_count++;
         }
 
-        if(Snake.score % 5 == 0 && Snake.score != 0 && food_count == 5){
-            if(Snake.speed <= 990){
+        //當分數=5的倍數時 => 加速蛇的移動
+        if(Snake.score % 5 == 0 && Snake.score != 0 && food_count == 5)
+        {
+            if(Snake.speed <= 990) //速度MAX = 990
+            {
                 Snake.speed += 10 ;
                 food_count = 0 ;
             }
         }
     }
 
-    if(gameOver){
+    if(gameOver)
+    {
         clearScreen();
         cout << "Such A Loser XD" << endl ;
+        cout << "Your score is :" << Snake.score << endl;
     }
+
     gotoXY(0,10);
-    system("pause");
+    system("pause"); //使遊戲視窗不會被關閉
     return 0;
 }
 
-void resetColor(){
+void resetColor()
+{
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, 7);
 }
-void gotoXY(int x, int y){
+
+void gotoXY(int x, int y)
+{
     HANDLE hStdOut;
     COORD coord;
-    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hStdOut == INVALID_HANDLE_VALUE) return;
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE); //從一個標準設備中取得句炳
+    if (hStdOut == INVALID_HANDLE_VALUE) return;         //調用失敗則返回
     coord.X = x ;
     coord.Y = y + 6;
-    SetConsoleCursorPosition(hStdOut, coord);
+    SetConsoleCursorPosition(hStdOut, coord); //設定游標位置
 }
 void clearScreen(){
     HANDLE                     hStdOut;
@@ -337,20 +372,22 @@ void clearScreen(){
             &count				//receives the number of characters written
     )) return;
 
-    /* Move the cursor home */
-    SetConsoleCursorPosition(hStdOut, homeCoords);
-}
+
 void setWindowSize(int width, int height){
-    HWND console = GetConsoleWindow();
+    HWND console = GetConsoleWindow(); //取得視窗的句炳
     RECT ConsoleRect;
     GetWindowRect(console, &ConsoleRect);
+    //取得RECT的指定尺寸(console讀入width、&ConsoleRect讀入height)
 
     MoveWindow(console, ConsoleRect.left, ConsoleRect.top, width, height, TRUE);
+    //改變指定視窗的位置和大小
 }
-void readInput(void* id){
+void readInput(void* id) //判斷使用者的input
+{
     char c;
+
     do{
-        c = getch();
+        c = getch(); //從鍵盤讀入任意字元
         switch(c){
             case 'w': direction = UP; break;
             case 'a': direction = LEFT; break;
@@ -358,11 +395,16 @@ void readInput(void* id){
             case 'd': direction = RIGHT; break ;
         }
     }while( (c != 'q') || c != (char)27);
-    _endthread();
+
+    _endthread(); //终止由 _beginthread所創建的線程
     return;
-    //cout << direction << endl ;
+
+    //cout << direction << endl ;  檢查判斷是否正確
 }
-void ShowConsoleCursor(bool showFlag){
+
+//隱藏游標
+void ShowConsoleCursor(bool showFlag)
+{
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
     CONSOLE_CURSOR_INFO     cursorInfo;
